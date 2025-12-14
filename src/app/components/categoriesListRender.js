55f76c46c80e06a9922c8044c15ef24ea093fcbf
Oleh <131261254+OlehDribko@users.noreply.github.com>
2025-12-14 18:19:40 +0100

    feat(OD-2): App refactoring , change logic, encapsulation (#2)
    Co-authored-by: Ваше ім'я <Oleh Dribko>

--- src/app/components/categoriesListRender.js
+++ src/app/components/categoriesListRender.js
@@ -1,10 +1,10 @@
-import { createDomElment } from "../utils.js";
+import { createDomElement } from "../utils.js";
 
-export default function categoriesListRender(categorysList) {
-  const categoryList = document.querySelector(".categories-list");
+export default function categoriesListRender(categoryList) {
+  const categoryListElement = document.querySelector(".categories-list");
 
-  categorysList.forEach((category, index) => {
-    const label = createDomElment("label", "category-label");
+  categoryList.forEach((category, index) => {
+    const label = createDomElement("label", "category-label");
 
     const input = document.createElement("input");
     input.type = "radio";
@@ -17,6 +17,6 @@ export default function categoriesListRender(categorysList) {
     if (index === 0) input.checked = true;
 
     label.append(input, span);
-    categoryList.append(label);
+    categoryListElement.append(label);
   });
 }
